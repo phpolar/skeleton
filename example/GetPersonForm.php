66b3dda8b767b9955e4b4e8544ebf025a081d51f
Eric Fortmeyer <efortmeyer@bethel.jw.org>
2023-06-23 09:41:56 -0500

    refactor: use phpolar 3.0.0a0
    BREAKING CHANGE: `App` will no longer load the DI container. Use `ContainerLoader` for now.

--- example/GetPersonForm.php
+++ example/GetPersonForm.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 namespace Phpolar\Example;
 
-use Phpolar\Phpolar\Model\Model;
-use Phpolar\Phpolar\Routing\AbstractContentDelegate;
+use Phpolar\Model\Model;
+use Phpolar\Phpolar\Http\RoutableInterface;
 use Phpolar\PurePhp\HtmlSafeContext;
 use Phpolar\PurePhp\TemplateEngine;
 use Psr\Container\ContainerInterface;
 
-final class GetPersonForm extends AbstractContentDelegate
+final class GetPersonForm implements RoutableInterface
 {
-    public function getResponseContent(ContainerInterface $container, #[Model] ?Person $person = null): string
+    public function process(ContainerInterface $container, #[Model] ?Person $person = null): string
     {
         $templateEngine = $container->get(TemplateEngine::class);
         $htmlSafe = new HtmlSafeContext($person);
