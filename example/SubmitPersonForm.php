ebb2e38bf4599eebc1e620e95d40c17f87aedc67
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2023-08-23 15:43:13 -0500

    fix: support phpolar 4.0.0

--- example/SubmitPersonForm.php
+++ example/SubmitPersonForm.php
@@ -9,33 +9,37 @@ use Phpolar\Routable\RoutableInterface;
 use Phpolar\Phpolar\Storage\AbstractStorage;
 use Phpolar\Phpolar\Storage\Item;
 use Phpolar\Phpolar\Storage\ItemKey;
+use Phpolar\PropertyInjector\Inject;
 use Phpolar\PurePhp\HtmlSafeContext;
 use Phpolar\PurePhp\TemplateEngine;
-use Psr\Container\ContainerInterface;
 
 final class SubmitPersonForm implements RoutableInterface
 {
-    public function process(ContainerInterface $container, #[Model] ?Person $person = null): string
+    #[Inject]
+    public TemplateEngine $templateEngine;
+
+    #[Inject]
+    public AbstractStorage $storage;
+
+    public function process(#[Model] ?Person $person = null): string
     {
-        $templateEngine = $container->get(TemplateEngine::class);
         if ($person->isValid() === true) {
             $person->create();
-            $this->savePerson($person, $container);
-            return $templateEngine->apply("example/templates/person-saved.phtml");
+            $this->savePerson($person);
+            return $this->templateEngine->apply("example/templates/person-saved.phtml");
         }
         $person->isPosted();
-        return $templateEngine->apply(
+        return $this->templateEngine->apply(
             "example/templates/add-person-form.phtml",
             new HtmlSafeContext($person),
         );
     }
 
-    private function savePerson(Person $person, ContainerInterface $container): void
+    private function savePerson(Person $person): void
     {
-        $storage = $container->get(AbstractStorage::class);
         $key = new ItemKey($person->id);
         $item = new Item($person);
-        $storage->storeByKey($key, $item);
-        $storage->commit();
+        $this->storage->storeByKey($key, $item);
+        $this->storage->commit();
     }
 }
--- src/config/dependencies/conf.d/routes.php
+++ src/config/dependencies/conf.d/routes.php
@@ -8,12 +8,19 @@ use Phpolar\Example\DeletePerson;
 use Phpolar\Example\GetPeople;
 use Phpolar\Example\GetPersonForm;
 use Phpolar\Example\SubmitPersonForm;
+use Phpolar\Phpolar\Http\RequestMethods;
 use Phpolar\Phpolar\Http\RouteMap;
+use Phpolar\PropertyInjector\PropertyInjector;
+use Psr\Container\ContainerInterface;
 
-$routes = new RouteMap();
-$routes->add("GET", "/", new GetPeople());
-$routes->add("GET", "/person/form", new GetPersonForm());
-$routes->add("POST", "/person/add", new SubmitPersonForm());
-$routes->add("POST", "/person/delete/{id}", new DeletePerson());
 
-return [RouteMap::class => $routes];
+return [
+    RouteMap::class => static function (ContainerInterface $container): RouteMap {
+        $routes = new RouteMap(new PropertyInjector($container));
+        $routes->add(RequestMethods::GET, "/", new GetPeople());
+        $routes->add(RequestMethods::GET, "/person/form", new GetPersonForm());
+        $routes->add(RequestMethods::POST, "/person/add", new SubmitPersonForm());
+        $routes->add(RequestMethods::POST, "/person/delete/{id}", new DeletePerson());
+        return $routes;
+    },
+];
